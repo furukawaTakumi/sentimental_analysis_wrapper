04ed1b0d8ff780686b315353686093392badceb9
菱田研究室 <hislab@hudson2021.local>
2021-05-22 22:42:27 +0900

    [change]change to dynamic import from static import

--- sentimental_analyzer/analyzers/__init__.py
+++ sentimental_analyzer/analyzers/__init__.py
@@ -1,7 +1,13 @@
 
 import os
+import re
 
-__all__ = [
-    'asari_analyzer',
-    'bert_analyzer'
-]
+__all__ = []
+python_file_regex = re.compile(r'.+\.py')
+
+for filename in os.listdir(f'{os.getcwd()}/sentimental_analyzer/analyzers'):
+    if not python_file_regex.match(filename):
+        continue
+    if '__init__.py' == filename:
+        continue
+    __all__.append(filename.split('.')[0])
