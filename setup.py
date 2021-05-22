a8ede0d9f61ea7b059662f65a846be6c21c52708
菱田研究室 <hislab@hudson2021.local>
2021-05-22 23:22:47 +0900

    [change]requirements.txt control dependencies

--- setup.py
+++ setup.py
@@ -1,18 +1,9 @@
-from setuptools import setup, find_packages
-
-install_requires = [
-    'pybind11',
-    'numpy',
-    'neologdn',
-    'asari'
-]
-
+from setuptools import setup
 
 
 setup(
     name='sentimental_analyzer',
     version='0.0.0',
-    install_requires=install_requires,
     packages=[
         'sentimental_analyzer'
     ]
